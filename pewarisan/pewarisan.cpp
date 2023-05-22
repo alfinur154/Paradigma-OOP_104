e886aa0de6465d41a36f213071835938166adb11
alfinur154 <alfinur27@gmail.com>
2023-05-22 11:48:54 +0700

    membuat main program

--- pewarisan/pewarisan.cpp
+++ pewarisan/pewarisan.cpp
@@ -7,8 +7,40 @@ class orang {
 	string nama;
 
 	orang(string pNama) :
-		nama(pNama),
-		sekolah(pSekolah) {
-		cout<<"pelajar"
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
 	}
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string Perkenalan() {
+		return "Hallo, nama saya" + nama + "Dari Sekolah" + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+	cout << siswa1.Perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
