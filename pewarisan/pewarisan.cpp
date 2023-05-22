cd2de30bd1b860750e62c0475a472cf112b3c6c4
alfinur154 <alfinur27@gmail.com>
2023-05-22 11:32:16 +0700

    constructor

--- pewarisan/pewarisan.cpp
+++ pewarisan/pewarisan.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
-using namespace;
+using namespace std;
+
+class orang {
+	public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama),
+		sekolah(pSekolah) {
+		cout<<"pelajar"
+	}
+};
